02e264fc04c53341648a50c5a6cba42657c08682
sanoj kumar <kumarsanoj2101@gmail.com>
2021-08-21 15:19:23 +0530

    implemanting array polyfill methods

---
+++ array-polyfill.js
@@ -0,0 +1,232 @@
+Array.prototype._forEach = function(callback) {
+    if (typeof callback !== "function") {
+        throw new Error(`${callback} is not a function`)
+    }
+    for(let i = 0; i < this.length; i++) {
+        callback(this[i], i, this);
+    }
+}
+
+Array.prototype._indexOf = function(element) {
+    for(let i = 0; i < this.length; i++) {
+        if(element === this[i]) {
+            return i;
+        } 
+    }
+    return -1;
+}
+// Assumption: pos will be integer number
+//TODO: where pos is negative number
+Array.prototype._splice = function(pos, noOfItemToDelete, ...itemsToadd) {
+    let diff = itemsToadd.length - noOfItemToDelete;
+    if (diff > 0) {
+        let i = this.length - 1;
+        while(i > pos) {
+            this[i+diff] = this[i];
+            i--;
+        }
+        let j = 0, k = 0;
+        let deletedItem = [];
+        for(i = pos; i < (pos + itemsToadd.length); i++) {
+            if (i < pos + noOfItemToDelete) {
+                deletedItem[k] = this[i];
+                k++;
+            }
+            this[i] = itemsToadd[j]
+            j++
+        }
+        return deletedItem;
+    } else {
+        let j = 0;
+        let i;
+        for(i =pos; i < (pos + itemsToadd); i++) {
+            this[i] = itemsToadd[j];
+            j++;
+        }
+        let diffInInteger = Math.abs(diff);
+        while(i + diffInInteger  < this.length) {
+            this[i] = this[i + diffInInteger];
+            i++;
+        }
+        this.length = this.length - diffInInteger;
+        return [];
+    }
+}
+
+Array.prototype._at = function(number) {
+    if (number > -1 && number < this.length) {
+        return this[number];
+    } else if(number < 0 && Math.abs(number) <= this.length) {
+        return this[this.length + number]
+    }
+}
+
+Array.prototype._concat = function(...arrList) {
+    let result = [...this];
+    for (let i = 0; i < arrList.length; i++) {
+        result = [...result, ...arrList[i]]
+    }
+    return result;
+}
+
+Array.prototype._copyWithin = function(target, start, end) {
+    let i = target;
+    let j = start;
+    while(i <= (target + (end - start))) {
+        this[i] = this[j];
+        i++;
+        j++;
+    }
+    return this;
+}
+
+Array.prototype._entries = function() {
+    let arr = this;
+    let i = 0;
+    return {
+        next: function() {
+            if (i < arr.length) {
+                let ret =  {
+                    value: [i, arr[i]],
+                    done: false
+                }
+                i = i+1;
+                return ret;
+            }
+            return {value: undefined, done: true}
+        }
+    }
+}
+
+Array.prototype._every = function(callback) {
+    if (typeof callback === 'function') {
+        for(let i=0; i < this.length; i++) {
+            let res = callback(this[i], i, this);
+            if (res === false) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+Array.prototype._filter = function(callback) {
+    if (typeof callback === 'function') {
+        let ret = []
+        for(let i=0; i < this.length; i++) {
+            let res = callback(this[i], i, this);
+            if (res === true) {
+                ret.push(this[i]);
+            }
+        }
+        return ret;
+    }
+}
+
+Array.prototype._find = function(callback) {
+    if (typeof callback === 'function') {
+        for(let i=0; i < this.length; i++) {
+            let res = callback(this[i], i, this);
+            if (res === true) {
+                return this[i];
+            }
+        }
+        
+    }
+}
+
+Array.prototype._findIndex = function(callback) {
+    if (typeof callback === 'function') {
+        for(let i=0; i < this.length; i++) {
+            let res = callback(this[i], i, this);
+            if (res === true) {
+                return i;
+            }
+        }
+        
+    }
+}
+
+Array.prototype._includes = function(ele) {
+    for(let i=0; i < this.length; i++) {
+        if (this[i] === ele) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Array.prototype._join = function(separator) {
+    let str = ''
+    let sep;
+    if (separator === '') {
+        sep='';
+    } else if (separator) {
+        sep=`${separator}`;
+    } else {
+        sep=',';
+    }
+    str = this[0]
+    for(let i=1; i < this.length; i++) {
+        str = str + sep +this[i]
+    }
+    return str;
+}
+
+Array.prototype._keys = function() {
+    let arr = this;
+    let i = 0;
+    return {
+        next: function() {
+            if (i < arr.length) {
+                let ret =  {
+                    value: i,
+                    done: false
+                }
+                i = i+1;
+                return ret;
+            }
+            return {value: undefined, done: true}
+        }
+    }
+}
+
+Array.prototype._map = function(callback) {
+    if (typeof callback === 'function') {
+        let ret = []
+        for(let i=0; i < this.length; i++) {
+            let res = callback(this[i], i, this);
+            ret.push(res);
+        }
+        return ret;
+    }
+}
+
+Array.prototype.reduce = function() {
+
+}
+
+Array.prototype.reduceRight = function() {
+    
+}
+Array.prototype.reverse = function() {
+    
+}
+Array.prototype.shift = function() {
+    
+}
+Array.prototype.slice = function() {
+    
+}
+Array.prototype.some = function() {
+    
+}
+Array.prototype.sort = function() {
+    
+}
+Array.prototype.toString = function() {
+    
+}
+Array.prototype.values = function() {
+    
+}
